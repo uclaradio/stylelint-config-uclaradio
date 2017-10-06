a6f1e495e2c6128ad1a826cc82cb0b1c5c08b8bc
Nathan Smith <nathan@nathansmith.io>
2017-10-06 08:54:00 -0700

    Init

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  extends: 'stylelint-config-recommended',
+  rules: {
+    'font-family-name-quotes': 'always-where-recommended',
+    'function-url-quotes': 'always',
+    'selector-attribute-quotes': 'always',
+    'string-quotes': 'double',
+  },
+};
